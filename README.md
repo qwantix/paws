9ce6ced6b0c82cde7576971baa8e813823c94636
Brice Dauzats <qwantix@gmail.com>
2018-01-21 22:44:44 +0100

    Initial import

---
+++ README.md
@@ -0,0 +1,105 @@
+# PAWS
+=================
+
+Simple AWS pricing cli
+
+
+## Install 
+
+```
+npm install -g paws-cli
+```
+
+## Usage
+
+List regions for EC2
+
+```
+> paws ec2 regions
+
+Available regions:
+ -  ap-northeast-1
+ -  ap-northeast-2
+ -  ap-south-1
+ -  ap-southeast-1
+ -  ap-southeast-2
+ -  ca-central-1
+ -  eu-central-1
+ -  eu-west-1
+ -  eu-west-2
+ -  eu-west-3
+ -  sa-east-1
+ -  us-east-1
+ -  us-east-2
+ -  us-gov-west-1
+ -  us-west-1
+ -  us-west-2
+
+```
+
+List search instances 
+```
+> paws ec2 ec2 instanceTypes -r eu-west-1 --family general
+
+Available instances:
+
+ -  m3.2xlarge
+ -  m3.large
+ -  m3.medium
+ -  m3.xlarge
+ -  m4.10xlarge
+ -  m4.16xlarge
+ -  m4.2xlarge
+ -  m4.4xlarge
+ -  m4.large
+ -  m4.xlarge
+ -  m5.12xlarge
+ -  m5.24xlarge
+ -  m5.2xlarge
+ -  m5.4xlarge
+ -  m5.large
+ -  m5.xlarge
+ -  t2.2xlarge
+ -  t2.large
+ -  t2.medium
+ -  t2.micro
+ -  t2.nano
+ -  t2.small
+```
+
+
+Quote service
+```
+> paws ec2 quote -r eu-west-1-i m3.medium --lease 1yr --upfront partial
+
+» Total Monthly: $40.88/month
+» Initial Fee: $468.00
+» Smoothed monthly: $79.88/month
+```
+
+Quote file
+```
+> paws quote sample.yml
+
+lambda myFooLambda
+──────────────────────────────────
+  » Total Monthly: $9.13/month
+
+ec2 server
+──────────────────────────────────
+  » Total Monthly: $40.88/month
+  » Initial Fee: $468.00
+  » Smoothed monthly: $79.88/month
+
+
+══════════════════════════════════
+TOTAL
+──────────────────────────────────
+  » Total Monthly: $50.01/month
+  » Initial Fee: $468.00
+  » Smoothed monthly: $89.01/month
+
+```
+
+Be patient, the first call may take some times
+
---
+++ bin/paws.js
@@ -0,0 +1,139 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const program = require('commander');
+const colors = require('colors');
+
+const services = require('../lib/services');
+const util = require('../lib/util');
+const pricing = require('../lib/pricing');
+const outputs = require('../lib/outputs');
+const quoteFile = require('../lib/quoteFile');
+
+program.version('1.0.0');
+
+// Little tricky override
+program.executables = true;
+program.executeSubCommand = function (argv, args, unknow) {
+  const topCommand = args.shift();
+  for (const c of this.commands) {
+    if (c._name === topCommand) {
+      return c.parse(Array(2).concat(args, unknow));
+    }
+  }
+  console.log('Command not found');
+  this.outputHelp();
+};
+
+function getParser(type) {
+  let args;
+  let typeName = type;
+  if (type instanceof Array) {
+    typeName = type[0];
+    args = type[1];
+  }
+  switch (typeName) {
+    case 'count':
+      return v => parseInt(v);
+    case 'duration':
+      return v => util.parseDuration(v, args || 's');
+    case 'bytes':
+      return v => util.parseBytes(v, args || 'k');
+    default:
+      return v => v;
+  }
+}
+
+function generateOption(cmd, name, def) {
+  let optName = `--${name}`;
+  if (def.short) {
+    optName = `-${def.short}, ${optName}`;
+  }
+  if (def.type !== 'bool') {
+    optName += def.required ? ' <value>' : ' [value]';
+  }
+
+  if (typeof def.values === 'function') {
+    def.values = def.values();
+  }
+
+  if (def.values) {
+    def.parser = v => ~def.values.indexOf(v) ? v : undefined;
+    def.description += ` (${def.values.join(', ')})`;
+  }
+
+  cmd.option(optName, def.description, def.parser || getParser(def.type), def.default || null);
+}
+
+Object.keys(services).forEach((serviceName) => {
+  const service = services[serviceName];
+
+
+  const serviceCmd = program.command(service.alias)
+    .description(`Service ${service.name}`);
+  // Register as executable
+  program._execs[serviceCmd._name] = true;
+
+  Object.keys(service.commands).forEach(name => {
+    const command = typeof service.commands[name] === 'function'
+      ? service.commands[name](service)
+      : service.commands[name];
+    const c = serviceCmd.command(name);
+    if (command.description) {
+      c.description('Informations about service');
+    }
+    for (const name in command.params) {
+      generateOption(c, name, command.params[name]);
+    }
+
+    generateOption(c, 'output', {
+      short: 'o',
+      description: 'Output',
+      values: ['json', 'pretty'],
+      default: 'pretty'
+    })
+
+    c.action((c) => {
+
+      for (const o of c.options) {
+        const name = o.attributeName();
+        const value = c[name];
+        if (o.required && ((typeof value === 'number' && isNaN(value)) || value === undefined || value === null)
+          || (!o.required && value !== o.defaultValue && value === undefined)) {
+          console.error(colors.red(`\n error: option --${o.name()} is invalid\n`));
+          c.outputHelp(colors.red);
+          process.exit(1);
+        }
+      }
+
+      const params = { output: c.output };
+      for (const name in command.params) {
+        params[name] = c[name];
+      }
+      const out = command.handler(params);
+      if (params.output === 'json') {
+        return console.log(JSON.stringify(out));
+      }
+      if (command.output && command.output.pretty) {
+        return command.output.pretty(out, params);
+      }
+      if (outputs[name] && outputs[name].pretty) {
+        return outputs[name].pretty(out, params);
+      }
+      console.error('Undefined output');
+    });
+
+    return c;
+  })
+
+
+});
+
+program.command('quote <file>')
+  .description('Quote from yaml file')
+  .action((file) => {
+    quoteFile(file)
+  })
+
+program.parse(process.argv);
